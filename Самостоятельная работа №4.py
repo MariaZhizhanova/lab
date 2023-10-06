6aecc92310c2c376e0d2c7abf92ff841a8d898e8
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-07 01:41:19 +0500

    Самостоятельная работа №4.py

---
+++ Самостоятельная работа №4.py
@@ -0,0 +1,20 @@
+```python
+def extract_employee_data(data, employee_id):
+    if employee_id not in data:
+        return tuple()  
+    
+    start_index = data.index(employee_id)
+    end_index = data[::-1].index(employee_id) 
+    end_index = len(data) - end_index  
+    
+    return data[start_index:end_index + 1]
+
+inputs = [((1, 2, 3), 8), ((1, 8, 3, 4, 8, 8, 9, 2), 8), ((1, 2, 8, 5, 1, 2, 9), 8)]
+expected_results = [(), (8, 3, 4, 8), (8, 5, 1, 2, 9)]
+
+for i in range(len(inputs)):
+    result = extract_employee_data(inputs[i][0], inputs[i][1])
+    print(f"Входные данные: {inputs[i][0]}, идентификатор сотрудника: {inputs[i][1]}")
+    print(f"Результат: {result}")
+    print(f"Ожидаемый результат: {expected_results[i]}\n")
+``
