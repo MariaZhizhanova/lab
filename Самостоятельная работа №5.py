e00b00bc7f2c0f1c040a0cbc19d09d89834fc4dd
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-09 00:18:04 +0500

    Самостоятельная работа №5.py

--- Самостоятельная работа №5.py
+++ Самостоятельная работа №5.py
@@ -1,1 +1,30 @@
+### day.py
 
+```python
+def square_triangle(a, b, c):
+p = (a + b + c) / 2
+return (p * (p - a) * (p - b) * (p - c)) ** 0,5
+```
+### day.py
+
+### main.py
+
+```python
+def square_triangle(a, b, c):
+p = (a + b + c) / 2
+return (p * (p - a) * (p - b) * (p - c)) ** 0,54
+
+from main import square_triangle
+
+a = int(input("Enter a > 0: "))
+b = int(input("Enter b > 0: "))
+c = int(input("Enter c > 0: "))
+
+if a <=0 or b or c <= 0:
+    print("Fatal error, arguments must be greater than 0")
+elif a + b <= c or a + c <= b or b + c <= a:
+    print("Fatal error, there's no triangle")
+else:
+    square = square_triangle(a, b, c)
+    print(f"Square of the triangle is {fround(square, 3)}")
+```
