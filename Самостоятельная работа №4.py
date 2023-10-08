6f26f871aba1f2b6890c981f867e7a16fcc37e1b
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-08 23:58:04 +0500

    Самостоятельная работа №4.py

--- Самостоятельная работа №4.py
+++ Самостоятельная работа №4.py
@@ -9,12 +9,11 @@ def extract_employee_data(data, employee_id):
     
     return data[start_index:end_index + 1]
 
-inputs = [((1, 2, 3), 8), ((1, 8, 3, 4, 8, 8, 9, 2), 8), ((1, 2, 8, 5, 1, 2, 9), 8)]
+inputs = [((1, 2, 3), 8), ((1, 8, 3, 4, 8, 8, 9, 2), 8), ((1, 2, 5, 1, 2,), 8)]
 expected_results = [(), (8, 3, 4, 8), (8, 5, 1, 2, 9)]
 
 for i in range(len(inputs)):
     result = extract_employee_data(inputs[i][0], inputs[i][1])
     print(f"Входные данные: {inputs[i][0]}, идентификатор сотрудника: {inputs[i][1]}")
-    print(f"Результат: {result}")
     print(f"Ожидаемый результат: {expected_results[i]}\n")
 ``
