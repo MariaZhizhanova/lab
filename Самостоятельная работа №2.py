ce8f4942cfb8b673a669ce0fc80f721d13f8e259
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-08 21:12:03 +0500

    Самостоятельная работа №2.py

--- Самостоятельная работа №2.py
+++ Самостоятельная работа №2.py
@@ -1,1 +1,16 @@
-
+```python
+def read_file (f_name):
+    try:
+        with open("пустой_файл.txt", "r") as file:
+            f = file.read()
+            if not f:
+                raise Exception("Файл пустой")
+            else:
+                print(f)
+    except FileNotFoundError:
+        print("Файл не найден")
+    except Exception as ex:
+         print(ex)
+f_name = "file.txt"
+read_file(f_name)
+```
