bd47a97830d42e0a181f06162f323c8c47d2e263
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-08 21:12:33 +0500

    Самостоятельная работа №3.py

--- Самостоятельная работа №3.py
+++ Самостоятельная работа №3.py
@@ -1,1 +1,14 @@
-
+```python
+def add_two():
+    user_input = input("Введите число: ")
+    try:
+        number = float(user_input)
+        return 2 + number
+    except (ValueError, TypeError):
+        raise ValueError("Неподходящий тип данных.")
+try:
+    result = add_two()
+    print(result)
+except ValueError as e:
+    print(e)
+```
