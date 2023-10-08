49bb0b1679b9606663ceee41535611cf5fdf1899
MariaZhizhanova <145640698+MariaZhizhanova@users.noreply.github.com>
2023-10-08 22:47:16 +0500

    Самостоятельная работа №1.py

--- Самостоятельная работа №1.py
+++ Самостоятельная работа №1.py
@@ -3,12 +3,18 @@ from datetime import datetime
 from math import sqrt
 
 def main(**kwargs):
-    for key in kwargs.items():
-        result = sqrt(key[1][0] ** 2 + key[1][1] ** 2) 
+    # Итерируемся по переданным ключ-значение аргументам
+    for key, value in kwargs.items():
+        # Вычисляем гипотенузу прямоугольного треугольника с заданными катетами
+        result = sqrt(value[0] ** 2 + value[1] ** 2) 
+        # Выводим результат
         print(result)
 
 if __name__ == '__main__':
+    # Засекаем время начала выполнения программы
     start_time = datetime.now()
+    
+    # Вызываем функцию main с передачей словаря аргументов
     main(
         one=[10, 3],
         two=[5, 4],
@@ -16,6 +22,11 @@ if __name__ == '__main__':
         four=[93, 53],
         five=[133, 15]
     )
+    
+    # Вычисляем время, затраченное на выполнение программы
     time_costs = datetime.now() - start_time
+    # Выводим время выполнения
     print(f"Время выполнения программы - {time_costs}")
+
 ```
+
